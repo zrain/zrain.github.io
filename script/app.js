47eb70f413fff8799aaf0663d17a4ad7adc08722
zrain <zrainma@sina.com>
2015-06-06 00:07:41 +0800

    zrain

---
+++ script/app.js
@@ -0,0 +1,38 @@
+define([
+    "angular",
+    "controller/controllers",
+    "directive/directives",
+    "service/services",
+    "route/routes"
+], function(angular, controllers, directives, services, routes){
+    "use strict";
+
+    var mainModule = angular.module("app", [
+            "ui.router",
+            "controllers",
+            "directives",
+            "services",
+            'ngSanitize',
+            "ngMessages",
+            "ngResource"
+        ]);
+        // '[]' 中代表依赖的模块
+        mainModule.config( function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider){
+            // 默认打开路径
+            // $urlRouterProvider.otherwise('guide');
+            // 注册路由
+            // for( var obj in routes ){
+            //     $stateProvider.state(
+            //         routes[obj].name,
+            //         {
+            //             url : routes[obj].route,
+            //             templateUrl : routes[obj].templateUrl,
+            //             title : routes[obj].title,
+            //             controller : routes[obj].controller,
+            //             views : routes[obj].views
+            //         }
+            //     );
+            // };
+            // $httpProvider.defaults.withCredentials = true; //CORS跨域支持的配置
+        });
+});
