eadba4d304da4bde6dcbb2bfcdfe8804f9faf7db
zrain <zrainma@sina.com>
2015-06-06 23:44:42 +0800

    zrain

---
+++ script/libs/statistics/baidu.js
@@ -0,0 +1,8 @@
+/*百度统计代码*/
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?5412020fcacaef0ab1fbfd3a120d83f8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
